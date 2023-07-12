a33aa85cd03cd86b3743045401da23eeb868421c
Shamnas Koyani <shamnas@loyalitsolutions.com>
2023-07-12 23:20:27 +0530

    sindal  data fetching

--- print3.py
+++ print3.py
@@ -17,14 +17,14 @@ CORS(app)
 def index():
 
     printer_data = request.form['printer_data']
-
+    # print(printer_data)
     datas = printer_data.split('\n')
     invoice_number = datas[7].strip()
     customer_name = [x for x in datas[8].split("  ") if x != ''][0].strip()
     customer_address = [x for x in datas[9].split("  ") if x != ''][0].strip()
     date = [x for x in datas[9].split("  ") if x != ''][1].strip()
     payment_terms = [x for x in datas[12].split("  ") if x != ''][0].strip()
-    sales_man = [x for x in datas[12].split("  ") if x != ''][1].strip()
+    sales_man = [x for x in datas[12].split("  ") if x != ''][0].strip()
 
 
     #find index of the list item which contains <img style=
@@ -33,24 +33,43 @@ def index():
             end_index = datas.index(data)
     line_items = []
     for i in range(16, end_index):
-        if (datas[i] != '    '):
-            alist = [x.strip() for x in datas[i].split("  ") if x != '']
-            dict = {
-                "sl_no":alist[0],
-                "item_name":alist[1],
-                "qty":alist[2],
-                "unit_price":alist[3],
-                "unit":alist[4],
-                "vat":alist[5],
-                "total":alist[6]
-            }
-            line_items.append(dict)
+        if (datas[i] != '    ' and datas[i] != ' ' and datas[i] != '' and datas[i] != '         ' ):
+            line_list = [x for x in datas[i].strip().split("  ") if x != '']
+            if len(line_list) == 7:
+                alist = [x.strip() for x in datas[i].split("  ") if x != '']
+                dict = {
+                    "sl_no":alist[0],
+                    "item_name":alist[1],
+                    "qty":alist[2],
+                    "unit_price":alist[3],
+                    "unit":alist[4],
+                    "vat":alist[5],
+                    "total":alist[6]
+                }
+                line_items.append(dict)
+            elif len(line_list) > 7:
+                n = len(line_list)
+                alist = [x.strip() for x in datas[i].split("  ") if x != '']
+                item_name = " ".join([item for item in alist[1:n-5]])
+                print(item_name)
+                dict = {
+                    "sl_no":alist[0],
+                    "item_name":item_name,
+                    "qty":alist[n-5],
+                    "unit_price":alist[n-4],
+                    "unit":alist[n-3],
+                    "vat":alist[n-2],
+                    "total":alist[n-1]
+                }
+                line_items.append(dict)
+
 
     image_binary = [x for x in datas[end_index].split("  ") if x != ''][0]
     total_in_words = [x for x in datas[end_index].split("  ") if x != ''][1]
     gross_value = [x for x in datas[end_index].split("  ") if x != ''][2]
     total_in_words += " " + [x for x in datas[end_index+1].split("  ") if x != ''][0].strip()
-    discount = [x for x in datas[end_index+1].split("  ") if x != ''][1].strip()
+    n = len([x for x in datas[end_index+1].split("  ") if x != '']) 
+    discount = [x for x in datas[end_index+1].split("  ") if x != ''][n-1].strip()
     delivery_date = [x for x in datas[end_index+2].split("  ") if x != ''][0].strip()
     total_case = [x for x in datas[end_index+2].split("  ") if x != ''][1].strip()
     total_weight = [x for x in datas[end_index+2].split("  ") if x != ''][2].strip()
