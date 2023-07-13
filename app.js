1451b9aaaebe5e9689b093361ea40a1185c2930a
Shamnas Koyani <shamnas@loyalitsolutions.com>
2023-07-13 05:58:31 +0530

    sindal paper setting

--- app.js
+++ app.js
@@ -4,15 +4,22 @@ const fs = require('fs');
 const { exec } = require('child_process');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const axios = require('axios');
+const { print } = require('pdf-to-printer');
+
 const wkhtmltopdf = require('wkhtmltopdf');
 
 const app = express();
 const port = 8000;
 
-app.use(cors());
+
+app.use(
+    cors()
+  );
+  
+// Parse JSON and urlencoded request bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.post('/dotmatrix/print', async (req, res) => {
   try {
@@ -20,46 +27,41 @@ app.post('/dotmatrix/print', async (req, res) => {
 
     const { printer_data } = req.body; // Assuming the HTML string is passed in the request body
 
+    //trim the string
+    const trimmedString = printer_data;
+
     // Add <pre> tags to the HTML string to preserve the formatting
-    const htmlStringWithPreTags = `<pre>${printer_data}</pre>`;
+    const htmlStringWithPreTags = `<pre style="font-weight:bold;">${trimmedString}</pre>`;
 
     console.log(printer_data);
 
     const pdfPath = './print.pdf'; // Specify the path and filename for the saved PDF file
 
     // Generate the PDF from the HTML string and save it to the file with pagesize A4 in landscape mode
-    wkhtmltopdf(htmlStringWithPreTags, { output: pdfPath, pageSize: "A4", orientation:"Landscape" }, error => {
-      if (error) {
-        console.error('PDF Conversion error:', error);
-        res.status(500).send('An error occurred while printing.');
-      } else {
-        console.log('PDF file saved successfully.');
 
-        // Read the PDF file as a buffer
-        const pdfBuffer = fs.readFileSync(pdfPath);
+    wkhtmltopdf(htmlStringWithPreTags, { output: pdfPath, orientation:"Landscape"}, error => {
+        if (error) {
+          console.error('PDF Conversion error:', error);
+        //   res.status(500).send('An error occurred while printing.');
+        } else {
+          console.log('PDF file saved successfully.');
+        }
+      });
 
-        // Send the PDF file to the Flask app
-        axios.post('http://localhost:8001/print', pdfBuffer, {
-          headers: {
-            'Content-Type': 'application/pdf'
-          }
-        })
-          .then(response => {
-            console.log('Print job sent to printer.');
-            res.status(200).send('Print job sent to printer.');
-          })
-          .catch(error => {
-            console.error('Printing error:', error);
-            res.status(500).send('An error occurred while printing.');
-          });
-      }
-    });
+    // Print the PDF file using pdf-to-printer
+    await print('./print.pdf', { orientation: 'landscape', paperSize: 'sindal' });
+
+    console.log('Print job sent to printer.');
+    // fs.unlinkSync(pdfPath)
+    res.status(200).send('Print job sent to printer.');
+
   } catch (error) {
-    console.error('Printing error:', error);
+    console.error('Printing error2:', error);
     res.status(500).send('An error occurred while printing.');
   }
 });
 
+
 app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+    console.log(`Server running on http://localhost:${port}`);
+  });
