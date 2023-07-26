9fe99685f72cdd2e163b9d72f5e4fc490b9c0780
Shamnas Koyani <shamnas@loyalitsolutions.com>
2023-07-26 13:37:53 +0530

    dynamic html creation

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const { exec } = require('child_process');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const { print } = require('pdf-to-printer');
+const ejs = require('ejs');
 
 const wkhtmltopdf = require('wkhtmltopdf');
 
 const app = express();
 const port = 8000;
 
+app.set('view engine', 'ejs');
 
 app.use(
     cors()
@@ -19,41 +21,66 @@ app.use(
 // Parse JSON and urlencoded request bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+  }
+);
 
 app.post('/dotmatrix/print', async (req, res) => {
   try {
     console.log('Printing request received.');
 
-    const { printer_data } = req.body; // Assuming the HTML string is passed in the request body
+    const data  = req.body['data']; // Assuming the HTML string is passed in the request body
 
-    //trim the string
-    const trimmedString = printer_data;
 
-    // Add <pre> tags to the HTML string to preserve the formatting
-    const htmlStringWithPreTags = `<html><head><meta charset="UTF-8"></head><body><pre style="font-weight:bold;">${trimmedString}</pre></body></html>`;
 
-    console.log(printer_data);
 
+    console.log(data);
     const pdfPath = './print.pdf'; // Specify the path and filename for the saved PDF file
 
-    // Generate the PDF from the HTML string and save it to the file with pagesize A4 in landscape mode
+    const QRCode = require('qrcode');
 
-    wkhtmltopdf(htmlStringWithPreTags, { output: pdfPath, orientation:"Landscape"}, error => {
+    const qc = data['qr_code_str'];
+    
+    console.log(qc);
+
+    QRCode.toDataURL(qc, function (err, url) {
+      data['qrcode_url'] = url;
+      if (err) {
+        QRCode.toDataURL("Error in getting qr code data", function (err, url) {
+          data['qrcode_url'] = url;
+        });
+        // console.error('Error generating QR code', err);
+        // return;
+      }
+
+      console.log(url);
+    });
+
+
+    ejs.renderFile('./views/print.ejs', data, (err, html) => {
+      console.log(html);
+      if (err) {
+        console.log(err);
+        res.status(500).send('An error occurred while creating html.');
+        return;
+      }
+      wkhtmltopdf(html, { output: pdfPath, orientation:"Landscape", disableSmartShrinking:false, enableExternalLinks:true}, error => {
         if (error) {
           console.error('PDF Conversion error:', error);
-        //   res.status(500).send('An error occurred while printing.');
+          res.status(500).send('An error occurred while printing.');
         } else {
           console.log('PDF file saved successfully.');
+          res.status(200).send('PDF created successfully.');
         }
       });
+    });
 
-    // Print the PDF file using pdf-to-printer
-    await print('./print.pdf', { orientation: 'landscape', paperSize: 'sindal' });
-
-    console.log('Print job sent to printer.');
-    // fs.unlinkSync(pdfPath)
-    res.status(200).send('Print job sent to printer.');
+    // await print('./print.pdf', { orientation: 'landscape', paperSize: 'sindal' });
+    // res.status(200).send('Print job sent to printer.');
 
   } catch (error) {
     console.error('Printing error2:', error);
