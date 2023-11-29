9e97510636316d610c5495d48a12703f90dac4dd
Shamnas Koyani <shamnas@loyalitsolutions.com>
2023-11-29 17:53:36 +0530

    Auto print code added

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ## Step 1
 
 ### Download Node.js for windows from the link below
+
 https://nodejs.org/dist/v18.17.0/node-v18.17.0-x64.msi
 
 ### Install Node.js from the downloaded package
@@ -10,8 +11,9 @@ https://nodejs.org/dist/v18.17.0/node-v18.17.0-x64.msi
 ## Step 2
 
 ### Clone the git repository
+
 ```
-git clone https://github.com/loyalcomps/node-print.git
+git clone https://github.com/loyalcomps/auto-print.git
 ```
 
 ## Step 3
@@ -19,12 +21,13 @@ git clone https://github.com/loyalcomps/node-print.git
 ### Open Command prompt and change directory to the cloned repository directory
 
 ```
-cd \path\to\the\node-print 
+cd \path\to\the\auto-print
 ```
 
 ## Step 4
 
 ### Install node package modules using npm
+
 ```
 npm install
 ```
@@ -36,7 +39,7 @@ npm install
 ### Open Command prompt and change directory to the cloned repository directory
 
 ```
-cd \path\to\the\node-print 
+cd \path\to\the\auto-print
 ```
 
 ## Step 2
@@ -48,3 +51,5 @@ node app.js
 ```
 
 # PS: Don't close the command prompt/ Terminal if you want to keep the print server running. :-)
+
+## Make sure to save the pdf files to the watch folder
--- app.js
+++ app.js
@@ -1,75 +1,35 @@
-const express = require('express');
 const FileSystem = require('fs');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const puppeteer = require('puppeteer');
-const QRCode = require('qrcode');
+const path = require('path');
 const { print } = require('pdf-to-printer');
 
-const app = express();
-const port = 8000;
+const watchFolder = './watch'; // Folder to watch for new PDF files
+const doneFolder = './done'; // Folder to move printed files
 
-app.set('view engine', 'ejs');
+// Ensure watch and done folders exist
+FileSystem.mkdirSync(watchFolder, { recursive: true });
+FileSystem.mkdirSync(doneFolder, { recursive: true });
 
-app.use(cors());
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.json());
-
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.post('/dotmatrix/print', async (req, res) => {
+// Function to print and move file
+async function printAndMoveFile(file) {
   try {
-    console.log('Printing request received.');
+    const filePath = path.join(watchFolder, file);
+    await print(filePath);
+    console.log(`Printed ${file}`);
 
-    const data = req.body['printer_data'];
-    const pdfPath = './print.pdf';
-    // console.log(data);
-    let print_data = JSON.parse(data);
-    const qc = print_data.qr_code_str;
-    // console.log(qc);
-    QRCode.toDataURL(qc, (err, url) => {
-        print_data.qrcode_url = url;
-      if (err) {
-        console.error('Error generating QR code', err);
-        return;
-      }
-
-      
-      
-      ejs.renderFile('./views/print.ejs', print_data, async (err, html) => {
-        if (err) {
-          console.error('Error rendering EJS template:', err);
-          res.status(500).send('An error occurred while creating html.');
-          return;
-        }
-
-        FileSystem.writeFileSync('./views/print.html', html);
-
-        const browser = await puppeteer.launch({headless: 'new',});
-        const page = await browser.newPage();
-        await page.setContent(html);
-        await page.pdf({ path: pdfPath, format: 'A4', landscape: true, printBackground: true});
-        await browser.close();
-
-        console.log('PDF file saved successfully.');
-        await print('./print.pdf', { orientation: 'landscape', paperSize: 'sindal' });
-        console.log('PDF print Job sent to the default printer successfully.');
-        res.status(200).send('PDF created successfully.');
-      });
-    });
-
+    const targetPath = path.join(doneFolder, file);
+    FileSystem.renameSync(filePath, targetPath);
+    console.log(`Moved ${file} to 'done' folder`);
   } catch (error) {
-    console.error('Printing error:', error);
-    res.status(500).send('An error occurred while printing.');
+    console.error(`Error handling ${file}:`, error);
+  }
+}
+
+// Watch for new files in the folder
+FileSystem.watch(watchFolder, (eventType, filename) => {
+  if (eventType === 'rename' && filename.endsWith('.pdf')) {
+    console.log(`New file detected: ${filename}`);
+    printAndMoveFile(filename);
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
-});
+console.log(`Watching for files in '${watchFolder}'...`);
